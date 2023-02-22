567301fca959edb8998205ffa7a2e0245f864f34
Agustin <aguspaparo@gmail.com>
2023-02-22 18:01:59 -0300

    Desplegar nuestro proyecto en la nube

---
+++ containers/containerUsuario.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import modelsUsuario from "../models/modelsUsuario.js";
+import logger from "../utils/logers.js";
+
+const MONGO = process.env.MONGO;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(
+	MONGO,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (err) {
+			logger.error(err);
+		} else {
+			logger.info("MongoDB Connected");
+		}
+	}
+);
+
+export default class Container {
+	async getUsuario(data) {
+		const usuario = await modelsUsuario.findOne({ username: data });
+		return usuario;
+	}
+
+	async addUsuario(data) {
+		const dataAdd = new modelsUsuario(data);
+		const add = await dataAdd.save();
+		return add;
+	}
+}
